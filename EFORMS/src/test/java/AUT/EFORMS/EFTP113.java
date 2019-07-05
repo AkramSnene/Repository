c013dc49b47fb3b3a1f791ad661704c2461654fc
AkramSnene <akramsnene@gmail.com>
2019-07-05 13:55:43 +0100

    update testng

--- EFORMS/src/test/java/AUT/EFORMS/EFTP113.java
+++ EFORMS/src/test/java/AUT/EFORMS/EFTP113.java
@@ -1,5 +1,8 @@
 package AUT.EFORMS;
 
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
@@ -7,15 +10,21 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.interactions.touch.TouchActions;
 import org.testng.annotations.*;
 import java.net.URL;
+import java.time.Duration;
 import java.net.MalformedURLException;
 import java.util.logging.Level;
+import static io.appium.java_client.touch.LongPressOptions.longPressOptions;
+import static io.appium.java_client.touch.offset.ElementOption.element;
+import io.appium.java_client.TouchAction;
 
-public class EFTP124 {
+public class EFTP113 {
 	  private String reportDirectory = "reports";
 	  private String reportFormat = "xml";
-	  private String testName = "EFTP124 Cliquer sur Quitter";
+	  private String testName = "EFTP113 Supprimer une photo";
 	  protected AndroidDriver<AndroidElement> driver = null;
 	  DesiredCapabilities dc = new DesiredCapabilities();
 	  
@@ -24,7 +33,7 @@ public class EFTP124 {
 	      dc.setCapability("reportDirectory", reportDirectory);
 	      dc.setCapability("reportFormat", reportFormat);
 	      dc.setCapability("testName", testName);
-	      dc.setCapability(MobileCapabilityType.UDID, "192.168.30.170:5555");
+	      dc.setCapability(MobileCapabilityType.UDID, "W3D7N16321018979");
 	      dc.setCapability("deviceName","P8 LITE");
 		  dc.setCapability("appPackage", "com.eforms.android"); 
 		  dc.setCapability("appActivity", "com.eforms.android.ui.activity.LauncherActivity");
@@ -32,7 +41,7 @@ public class EFTP124 {
 	      driver.setLogLevel(Level.INFO);
 	  }
 	  @Test
-	  public void TEST124() {
+	  public void TEST113() {
 	  new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='password']")));
 	  driver.findElement(By.xpath("//*[@id='forms_spinner']")).click();
 	  driver.findElement(By.xpath("//*[@text='Finky-acc']")).click();
@@ -62,33 +71,35 @@ public class EFTP124 {
       driver.findElement(By.xpath("//*[@text='Validation']")).click();
       new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@text='eForms']")));  
       
-      driver.findElement(By.xpath("//*[@id='parking_spinner']")).click();
-      new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@text='Disque non conforme']"))); 
-      driver.findElement(By.xpath("//*[@text='Disque non conforme']")).click();
+      driver.findElement(By.xpath("//*[@id='take_photo_view']")).click();
+      try{Thread.sleep(2000);} catch(Exception ignore){}
       
-      driver.findElement(By.xpath("//*[@id='brand_searchable_spinner']")).click();
-      driver.findElement(By.xpath("//*[@text='Audi']")).click();
+      new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@resource-id='com.eforms.android:id/camera_photo_button']")));  
+      driver.findElement(By.xpath("//*[@resource-id='com.eforms.android:id/camera_photo_button']")).click();
+      try{Thread.sleep(2000);} catch(Exception ignore){}
+      driver.findElement(By.xpath("//*[@resource-id='com.eforms.android:id/camera_back_button']")).click();
       
-      driver.findElement(By.xpath("//*[@id='color_spinner']")).click();
-      driver.executeScript("seetest:client.swipeWhileNotFound(\"Down\", 150, 2000, \"NATIVE\", \"//*[@text='BRUN']\", 0, 1000, 7, false)");
-      driver.findElement(By.xpath("//*[@text='BRUN']")).click();
-      
-      driver.findElement(By.xpath("//*[@text='Suivant']")).click();
+      new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='picture_img']"))); 
       
       try{Thread.sleep(2000);} catch(Exception ignore){}
-      new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='comment_edit_text']"))); 
-      driver.findElement(By.xpath("//*[@id='comment_edit_text']")).sendKeys("123");
       
-      driver.findElement(By.xpath("//*[@text='Validation']")).click();
-      new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@text='Imprimante non configurée, continuer sans imprimer ?']"))); 
+      //TouchAction action = new TouchAction(driver).longPress(longPressOptions().withElement(driver.findElement(By.xpath("//*[@resource-id='com.eforms.android:id/camera_back_button']"))).withDuration(Duration.ofMillis(10000))).release().perform();
+      //Thread.sleep(5000);
       
-      driver.findElement(By.xpath("//*[@text='Oui']")).click();
-      new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@text='Imprimer']"))); 
-      new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@text='Suivant']"))); 
+      driver.findElementByAccessibilityId("//*[@id='picture_img']").click();
+      MobileElement longpress = (MobileElement) new WebDriverWait(driver, 30).
+              until(ExpectedConditions.elementToBeClickable(MobileBy.AccessibilityId("//*[@id='picture_img']")));
+      new Actions(driver).clickAndHold(longpress).perform();
+      try{Thread.sleep(2000);} catch(Exception ignore){}
+      ///TouchActions action = new TouchActions(driver);
+      //action.longPress(driver.findElement(By.xpath("//*[@resource-id='com.eforms.android:id/camera_back_button']")));
+      //action.perform();
+      ///action.clickAndHold(driver.findElement(By.xpath("//*[@resource-id='com.eforms.android:id/camera_back_button']")));
+      try{Thread.sleep(2000);} catch(Exception ignore){}
       
-      driver.findElement(By.xpath("//*[@text='Suivant']")).click();
+      driver.findElement(By.xpath("//*[@resource-id='com.eforms.android:id/delete_all_photo_view']")).click();
 
-      new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@text=concat('Reconnaitre la plaque. L', \"'\", 'opération peut prendre quelques secondes')]"))); 
+      
       
       try{Thread.sleep(2000);} catch(Exception ignore){}
 	  System.out.println("==> TEST_PASSED <==");
